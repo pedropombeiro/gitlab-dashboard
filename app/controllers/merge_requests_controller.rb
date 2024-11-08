fc07ff260e3f1f78cdf06b605173bedece508326
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-08 20:00:19 +0100

    Move GitLab API calls to concern

--- app/controllers/merge_requests_controller.rb
+++ app/controllers/merge_requests_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require "async"
-require "ostruct"
 
 class MergeRequestsController < ApplicationController
   include CacheConcern
+  include GitlabApiConcern
 
   MR_ISSUE_PATTERN = %r{[^\d]*(?<issue_id>\d+)[/-].+}i.freeze
 
@@ -39,62 +39,14 @@ class MergeRequestsController < ApplicationController
   OPEN_MRS_CONTEXTUAL_LABELS = ["pipeline::"].freeze
   MERGED_MRS_CONTEXTUAL_LABELS = (DEPLOYMENT_LABELS + WORKFLOW_LABELS).freeze
 
-  CORE_USER_FRAGMENT = <<-GRAPHQL
-      fragment CoreUserFields on User {
-        username
-        avatarUrl
-        webUrl
-      }
-    GRAPHQL
-
-  EXT_USER_FRAGMENT = <<-GRAPHQL
-      fragment ExtendedUserFields on User {
-        ...CoreUserFields
-        lastActivityOn
-        location
-        status {
-          availability
-          message
-        }
-      }
-    GRAPHQL
-
-  CORE_ISSUE_FRAGMENT = <<-GRAPHQL
-      fragment CoreIssueFields on Issue {
-        iid
-        webUrl
-        titleHtml
-      }
-    GRAPHQL
-
-  CORE_MERGE_REQUEST_FRAGMENT = <<-GRAPHQL
-      fragment CoreMergeRequestFields on MergeRequest {
-        iid
-        webUrl
-        titleHtml
-        reference
-        sourceBranch
-        targetBranch
-        createdAt
-        updatedAt
-        assignees {
-          nodes { ...CoreUserFields }
-        }
-        labels {
-          nodes {
-            title
-            descriptionHtml
-            color
-            textColor
-          }
-        }
-      }
-    GRAPHQL
-
   helper_method :humanized_enum, :make_full_url, :user_help_title, :reviewer_help_title
 
   def index
-    @user = fetch_user(params[:assignee])
+    assignee = params[:assignee]
+    @user = Rails.cache.fetch(user_cache_key(assignee), expires_in: USER_CACHE_VALIDITY) do
+      fetch_user(assignee)
+    end.data.user
+
     unless params[:assignee] || ENV["GITLAB_TOKEN"]
       return render(status: :network_authentication_required, plain: "Please configure GITLAB_TOKEN to use default user")
     end
@@ -140,28 +92,6 @@ class MergeRequestsController < ApplicationController
 
   private
 
-  def user_hash(username)
-    Digest::SHA256.hexdigest(username || ENV["GITLAB_TOKEN"] || "Anonymous")[0..15]
-  end
-
-  def fetch_user(username)
-    response = Rails.cache.fetch(user_cache_key(username), expires_in: USER_CACHE_VALIDITY) do
-      response = client.query <<-GRAPHQL
-        query {
-          user: #{username ? "user(username: \"#{username}\")" : "currentUser"} {
-            ...CoreUserFields
-          }
-        }
-
-        #{CORE_USER_FRAGMENT}
-      GRAPHQL
-
-      make_serializable(response)
-    end
-
-    response.data.user
-  end
-
   def render_404
     respond_to do |format|
       format.html { render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found }
@@ -170,174 +100,8 @@ class MergeRequestsController < ApplicationController
     end
   end
 
-  def gitlab_instance_url
-    @gitlab_instance_url ||= ENV.fetch("GITLAB_URL", "https://gitlab.com")
-  end
-
-  def authorization
-    "Bearer #{ENV["GITLAB_TOKEN"]}"
-  end
-
-  def client
-    ::Graphlient::Client.new(
-      "#{gitlab_instance_url}/api/graphql",
-      headers: { "Authorization" => authorization },
-      http_options: {
-        read_timeout: 20,
-        write_timeout: 30
-      }
-    )
-  end
-
   def parse_graphql_time(timestamp)
     Time.parse(timestamp) if timestamp
-  end
-
-  def fetch_open_merge_requests(username)
-    merge_requests_graphql_query = <<-GRAPHQL
-      query($username: String!, $activeReviewsAfter: Time) {
-        user(username: $username) {
-          openMergeRequests: authoredMergeRequests(state: opened, sort: UPDATED_DESC) {
-            nodes {
-              ...CoreMergeRequestFields
-              approved
-              approvalsRequired
-              approvalsLeft
-              autoMergeEnabled
-              detailedMergeStatus
-              squashOnMerge
-              conflicts
-              blockingMergeRequests {
-                visibleMergeRequests {
-                  state
-                }
-              }
-              reviewers {
-                nodes {
-                  ...ExtendedUserFields
-                  mergeRequestInteraction {
-                    approved
-                    reviewState
-                  }
-                  activeReviews: reviewRequestedMergeRequests(
-                    state: opened, approved: false, updatedAfter: $activeReviewsAfter
-                  ) {
-                    count
-                  }
-                }
-              }
-              headPipeline {
-                path
-                startedAt
-                finishedAt
-                status
-                failureReason
-                jobs(retried: false) {
-                  count
-                }
-                finishedJobs: jobs(statuses: [SUCCESS, FAILED, CANCELED, SKIPPED], retried: false) {
-                  count
-                }
-                runningJobs: jobs(statuses: RUNNING, retried: false) {
-                  count
-                }
-                firstRunningJob: jobs(statuses: RUNNING, first: 1, retried: false) {
-                  nodes {
-                    webPath
-                  }
-                }
-                failedJobs: jobs(statuses: FAILED, retried: false) {
-                  count
-                  nodes {
-                    name
-                  }
-                }
-                failedJobTraces: jobs(statuses: FAILED, first: 1, retried: false) {
-                  nodes {
-                    name
-                    trace { htmlSummary }
-                    webPath
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      #{CORE_USER_FRAGMENT}
-      #{EXT_USER_FRAGMENT}
-      #{CORE_MERGE_REQUEST_FRAGMENT}
-    GRAPHQL
-
-    response = client.query(merge_requests_graphql_query, username: username, activeReviewsAfter: 7.days.ago)
-
-    OpenStruct.new(
-      user: make_serializable(response.data.user),
-      updated_at: Time.current
-    )
-  end
-
-  def fetch_merged_merge_requests(username)
-    merge_requests_graphql_query = <<-GRAPHQL
-      query($username: String!) {
-        user(username: $username) {
-          mergedMergeRequests: authoredMergeRequests(state: merged, sort: MERGED_AT_DESC, first: 20) {
-            nodes {
-              iid
-              ...CoreMergeRequestFields
-              mergedAt
-              mergeUser {
-                ...ExtendedUserFields
-              }
-            }
-          }
-        }
-      }
-
-      #{CORE_USER_FRAGMENT}
-      #{EXT_USER_FRAGMENT}
-      #{CORE_MERGE_REQUEST_FRAGMENT}
-    GRAPHQL
-
-    response = client.query(merge_requests_graphql_query, username: username)
-
-    OpenStruct.new(
-      user: make_serializable(response.data.user),
-      updated_at: Time.current
-    )
-  end
-
-  def fetch_issues(merged_mr_issue_iids, open_mr_issue_iids)
-    query = <<-GRAPHQL
-      query($projectPath : ID!, $issueIids: [String!], $openIssueIids: [String!]) {
-        project(fullPath: $projectPath) {
-          issues(iids: $issueIids) {
-            nodes { ...CoreIssueFields }
-          }
-          openIssues: issues(iids: $openIssueIids, state: opened) {
-            nodes { ...CoreIssueFields }
-          }
-        }
-      }
-
-      #{CORE_ISSUE_FRAGMENT}
-    GRAPHQL
-
-    response = client.query(
-      query,
-      projectPath: "gitlab-org/gitlab",
-      issueIids: open_mr_issue_iids,
-      openIssueIids: merged_mr_issue_iids # Only fetch open issues for merged MRs
-    )
-
-    project = make_serializable(response.data.project)
-    project.issues.nodes + project.openIssues.nodes
-  end
-
-  def make_serializable(obj)
-    # GraphQL types cannot be serialized, so we work around that by reparsing from JSON into anonymous objects
-    JSON.parse!(obj.to_json, object_class: OpenStruct)
   end
 
   def convert_mr_pipeline(pipeline)
