162cbb8a9c325f689ad30e459ffcd591f2796f7a
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-13 23:34:22 +0100

    Create stimulus controller for clipboard action

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import ClipboardController from "./clipboard_controller"
+application.register("clipboard", ClipboardController)
+
 import WebPushController from "./web_push_controller"
 application.register("web-push", WebPushController)
