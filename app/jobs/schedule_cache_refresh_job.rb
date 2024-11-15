50ccf4e4b43ae463f5ae8c3f56cf93f709ae09b6
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-15 17:24:09 +0100

    Limit background fetch to 10 users
    Avoid DoS attacks

--- app/jobs/schedule_cache_refresh_job.rb
+++ app/jobs/schedule_cache_refresh_job.rb
@@ -6,8 +6,17 @@ class ScheduleCacheRefreshJob < ApplicationJob
   ACTIVE_USER_TIME_WINDOW = 4.hours
 
   def perform(*_args)
-    GitlabUser.where(GitlabUser.arel_table[:contacted_at].gteq(ACTIVE_USER_TIME_WINDOW.ago)).each do |user|
+    scope.each do |user|
       MergeRequestsFetchJob.perform_later(user.username)
     end
+  end
+
+  private
+
+  def scope
+    GitlabUser
+      .where(GitlabUser.arel_table[:contacted_at].gteq(ACTIVE_USER_TIME_WINDOW.ago))
+      .order(contacted_at: :desc)
+      .limit(10) # Limit to 10 users for the time being, to avoid any DoS attacks
   end
 end
