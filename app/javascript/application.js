4386c8b9ebab1bc4c0f459b47e1a4551b6dc2a70
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-22 00:18:41 +0100

    Leverage local-time JS library and gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-import "@fortawesome/fontawesome-free/js/all"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import * as bootstrap from "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
+
+import LocalTime from "local-time";
+LocalTime.start();
+LocalTime.config.useFormat24 = true
