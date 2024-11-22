cc3170bf832fb66d404cb01a8d60f1578dcb7629
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-22 09:57:14 +0100

    Fix JS warning

--- app/javascript/controllers/bootstrap_tooltip_controller.js
+++ app/javascript/controllers/bootstrap_tooltip_controller.js
@@ -4,8 +4,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     setTimeout(() => { // Need to wait for 500 ms since relative_timestamp sets data-bs-toggle class a bit too late
-      var tooltipTriggerList = this.element.querySelectorAll('[data-bs-toggle="tooltip"]')
-      tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+      var tooltipTriggerList = this.element.querySelectorAll('[data-bs-toggle="tooltip"]');
+      [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
     }, 500);
   }
 }
