ca81ccdf080451eea7ea06c3b0f168c2f88b2aeb
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-26 22:48:26 +0100

    Add RSpec and FactoryBot support

---
+++ spec/factories/gitlab_users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :gitlab_user do
+    id { 1 }
+    username { "MyText" }
+    created_at { "2024-11-26 22:06:06" }
+    updated_at { "2024-11-26 22:06:06" }
+    contacted_at { "2024-11-26 22:06:06" }
+  end
+end
