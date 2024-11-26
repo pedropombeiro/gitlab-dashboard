724faf4f4608c0b1965bf6fdeaccc90fb0b092da
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-26 22:48:26 +0100

    Add RSpec and FactoryBot support

---
+++ spec/factories/web_push_subscriptions.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :web_push_subscription do
+    gitlab_user
+    endpoint { "endpoint" }
+    auth_key { "authkey" }
+    p256dh_key { "p256dh_key" }
+    user_agent { "user_agent" }
+    created_at { "2024-11-26 22:06:23" }
+    updated_at { "2024-11-26 22:06:23" }
+  end
+end
---
+++ spec/models/gitlab_user_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+RSpec.describe GitlabUser, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
