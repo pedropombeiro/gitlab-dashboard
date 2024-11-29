7c13d699b85515bb782fcafb29b86e5f555cad4e
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-29 18:51:12 +0100

    Update bootstrap js to 5.3.3 and use CDN except for CSS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
-import "./controllers";
-import * as bootstrap from "bootstrap";
+//import * as bootstrap from "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
+import "./controllers";
 
 import LocalTime from "local-time";
 LocalTime.config.useFormat24 = true
