86a878720c357f252a97e99cbbe70ab4ed9e58bd
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-01 17:41:34 +0100

    Test rendered /index view

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,10 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
+  config.include StubEnv::Helpers
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_paths = [
   #   Rails.root.join("spec/fixtures")
