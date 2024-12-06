9d300ad73d832cd5685479e0286d85d4763f27e7
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-06 16:37:29 +0100

    Only cache responses in production

--- app/controllers/merge_requests_controller.rb
+++ app/controllers/merge_requests_controller.rb
@@ -52,7 +52,9 @@ class MergeRequestsController < MergeRequestsControllerBase
     end
 
     fresh_when(response)
-    expires_in(Services::FetchMergeRequestsService::MR_CACHE_VALIDITY.after(response.updated_at) - Time.current)
+    if Rails.env.production?
+      expires_in(Services::FetchMergeRequestsService::MR_CACHE_VALIDITY.after(response.updated_at) - Time.current)
+    end
 
     check_changes(previous_dto, @dto) if current_user.web_push_subscriptions.any?
 
