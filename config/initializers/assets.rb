472e39344a8523ac97ade26381b81ed80a912bc8
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-07 16:00:05 +0100

    Use CDN for flag-icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
-Rails.application.config.assets.paths << Rails.root.join("node_modules/flag-icons")
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # Precompile additional assets.
 # Rails.application.config.assets.precompile << "bootstrap.bundle.min.js"
