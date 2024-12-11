c01fa1324dd1364f4b309bb1b179d20ecf1a30d5
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-11 23:57:43 +0100

    Create AutoRefresh stimulus controller

---
+++ app/javascript/controllers/auto_refresh_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="auto-refresh"
+export default class extends Controller {
+  static values = { timeout: Number, targetDomId: String };
+
+  controller = new AbortController();
+
+  connect() {
+    console.log("Auto refresh controller connected to", this.targetDomIdValue);
+
+    const onVisibilityChange = () => {
+      if (document.visibilityState === "visible") {
+        console.log("Auto refreshing after page made visible");
+        this.refresh();
+      }
+    };
+
+    document.addEventListener("visibilitychange", onVisibilityChange, false, { signal: this.controller.signal });
+
+    setTimeout(this.refresh, this.timeoutValue);
+  }
+
+  disconnect() {
+    this.controller.abort();
+  }
+
+  refresh() {
+    const element = document.getElementById(this.targetDomIdValue);
+    if (element) {
+      element.reload();
+    }
+  }
+}
