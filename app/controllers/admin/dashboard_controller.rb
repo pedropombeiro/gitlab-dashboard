3098ed2e56d4551ffef1383098bbbd6b71e1c523
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-12 22:04:17 +0100

    Add "Next Scheduled Update" column in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,18 @@
 class Admin::DashboardController < ApplicationController
+  include CacheConcern
+
+  helper_method :user_cache_validity
+
   def index
     @recent_users = GitlabUser.recent
+  end
+
+  private
+
+  def user_cache_validity(assignee)
+    response = Rails.cache.read(self.class.last_authored_mr_lists_cache_key(assignee))
+    return unless response&.next_scheduled_update_at
+
+    response.next_scheduled_update_at
   end
 end
