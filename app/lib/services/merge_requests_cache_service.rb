aba500531b95d825ad8db7c26136fee3d5e5deeb
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-12 22:23:36 +0100

    Clean up code with specialized MR cache service

---
+++ app/lib/services/merge_requests_cache_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "async"
+
+module Services
+  class MergeRequestsCacheService
+    include CacheConcern
+
+    def self.cache_validity
+      if Rails.application.config.action_controller.perform_caching
+        MR_CACHE_VALIDITY
+      else
+        1.minute
+      end
+    end
+
+    def needs_scheduled_update?(assignee)
+      response = read(assignee)
+      return true unless response&.next_scheduled_update_at
+
+      response.next_scheduled_update_at.past?
+    end
+
+    def read(assignee)
+      Rails.cache.read(self.class.last_authored_mr_lists_cache_key(assignee))
+    end
+
+    def write(assignee, response)
+      Rails.cache.write(self.class.last_authored_mr_lists_cache_key(assignee), response, expires_in: 1.week)
+    end
+  end
+end
