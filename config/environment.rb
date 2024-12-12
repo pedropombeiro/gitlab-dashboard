78a66117631a9b69186863d66c3c42b719956adc
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-12 18:13:10 +0100

    Fix Rails.application.routes.default_url_options

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.routes.default_url_options = GitlabDashboard::Application.config.action_mailer.default_url_options
