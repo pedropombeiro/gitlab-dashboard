edda2de4db24862b39d7265172134ebacc4108ad
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-13 22:07:06 +0100

    Code cleanup

--- app/lib/helpers/merge_requests_parsing_helper.rb
+++ app/lib/helpers/merge_requests_parsing_helper.rb
@@ -2,6 +2,7 @@
 
 module MergeRequestsParsingHelper
   MR_ISSUE_PATTERN = %r{[^\d]*(?<issue_id>\d+)[/-].+}i
+  SECURITY_SUBGROUP = "/security"
 
   def issue_from_mr(mr, open_issues_by_iid)
     iid = issue_iid_from_mr(mr)
@@ -14,18 +15,27 @@ module MergeRequestsParsingHelper
   end
 
   def merge_request_issue_iids(merge_requests)
-    merge_requests.flat_map do |mr|
-      [
-        {
-          project_full_path: mr.project.fullPath,
-          issue_iid: issue_iid_from_mr(mr)
-        },
-        {
-          # Take into account MRs in security projects which refer to issues in the canonical project
-          project_full_path: mr.project.fullPath.sub("/security", ""),
-          issue_iid: issue_iid_from_mr(mr)
-        }
-      ].uniq
+    merge_requests.flat_map { |mr| issue_references(mr) }
+  end
+
+  private
+
+  def issue_references(mr)
+    refs = []
+
+    refs << {
+      project_full_path: mr.project.fullPath,
+      issue_iid: issue_iid_from_mr(mr)
+    }
+
+    if mr.project.fullPath.include?(SECURITY_SUBGROUP)
+      refs << {
+        # Take into account MRs in security projects which refer to issues in the canonical project
+        project_full_path: mr.project.fullPath.delete(SECURITY_SUBGROUP),
+        issue_iid: issue_iid_from_mr(mr)
+      }
     end
+
+    refs
   end
 end
