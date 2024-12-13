0feedf033383aadfb9adb6e5f7dbb0b83b69b598
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-13 14:32:14 +0100

    Log to production.log when in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,10 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [:request_id]
-  config.logger = ActiveSupport::TaggedLogging.logger($stdout)
+  config.logger = ActiveSupport::BroadcastLogger.new(
+    ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout, formatter: Logger::Formatter.new)),
+    ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new("log/production.log", formatter: Logger::Formatter.new))
+  )
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
