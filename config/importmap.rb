923d77c62a0ee940ea43b84f1884b0f267bd9fb1
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-15 00:40:51 +0100

    Run bin/importmap pin bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "local-time" # @3.0.2
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
