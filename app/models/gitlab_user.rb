213a946e5aeaac5fd8aab3f9929b150f9f8e0fc4
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-16 09:53:15 +0100

    Add more details to Honeybadger user metrics

--- app/models/gitlab_user.rb
+++ app/models/gitlab_user.rb
@@ -3,27 +3,34 @@ class GitlabUser < ApplicationRecord
 
   scope :recent, -> { order(contacted_at: :desc).limit(10) }
 
-  def self.safe_find_or_create_by(*args, &block)
-    record = find_by(*args)
-    return record if record.present?
+  class << self
+    include Honeybadger::InstrumentationHelper
 
-    Honeybadger.increment_counter("user.create", args)
+    def safe_find_or_create_by(*args, &block)
+      record = find_by(*args)
+      return record if record.present?
 
-    # We need to use `all.create` to make this implementation follow `find_or_create_by` which delegates this in
-    # https://github.com/rails/rails/blob/v6.1.3.2/activerecord/lib/active_record/querying.rb#L22
-    #
-    # When calling this method on an association, just calling `self.create` would call `ActiveRecord::Persistence.create`
-    # and that skips some code that adds the newly created record to the association.
-    transaction(requires_new: true) { all.create(*args, &block) } # rubocop:disable Performance/ActiveRecordSubtransactions
-  rescue ActiveRecord::RecordNotUnique
-    find_by(*args)
-  end
+      metric_source "custom_metrics"
+      metric_attributes(**args.last.slice(:username))
 
-  def self.safe_find_or_create_by!(*args, &block)
-    safe_find_or_create_by(*args, &block).tap do |record|
-      raise ActiveRecord::RecordNotFound if record.blank?
+      Honeybadger.increment_counter("user.create")
 
-      record.validate! unless record.persisted?
+      # We need to use `all.create` to make this implementation follow `find_or_create_by` which delegates this in
+      # https://github.com/rails/rails/blob/v6.1.3.2/activerecord/lib/active_record/querying.rb#L22
+      #
+      # When calling this method on an association, just calling `self.create` would call `ActiveRecord::Persistence.create`
+      # and that skips some code that adds the newly created record to the association.
+      transaction(requires_new: true) { all.create(*args, &block) } # rubocop:disable Performance/ActiveRecordSubtransactions
+    rescue ActiveRecord::RecordNotUnique
+      find_by(*args)
+    end
+
+    def safe_find_or_create_by!(*args, &block)
+      safe_find_or_create_by(*args, &block).tap do |record|
+        raise ActiveRecord::RecordNotFound if record.blank?
+
+        record.validate! unless record.persisted?
+      end
     end
   end
 end
