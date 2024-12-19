f61e2a91d80d2fbbcaeaf99a8d588bccdde179d5
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-19 19:14:46 +0100

    Change routing so that usernames with special characters can be used

--- spec/routing/merge_requests_spec.rb
+++ spec/routing/merge_requests_spec.rb
@@ -2,21 +2,14 @@ require "rails_helper"
 
 RSpec.describe "routes for Merge Requests", type: :routing do
   it "routes / to the merge requests controller" do
-    expect(get("/"))
-      .to route_to("merge_requests#index")
+    expect(get("/")).to route_to("merge_requests#index")
   end
 
-  it "does not route /mrs" do
-    expect(get("/mrs")).not_to be_routable
+  it "routes /mrs to the merge requests controller" do
+    expect(get("/mrs")).to route_to("merge_requests#index")
   end
 
-  it "routes /mrs/:assignee to the merge requests controller" do
-    expect(get("/mrs/:assignee"))
-      .to route_to("merge_requests#index", "assignee" => ":assignee")
-  end
-
-  it "routes /mrs/:assignee/list to the merge requests controller" do
-    expect(get("/mrs/:assignee/list"))
-      .to route_to("merge_requests#list", "assignee" => ":assignee")
+  it "routes /mrs/list to the merge requests controller" do
+    expect(get("/mrs/list")).to route_to("merge_requests#list")
   end
 end
