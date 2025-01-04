aa0abcb3e08a45adafcfdd6e2b8c9a12acd3ce90
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-01-04 15:27:59 +0100

    Replace speech_balloon by message if message only contains one emoji

--- app/helpers/merge_requests_helper.rb
+++ app/helpers/merge_requests_helper.rb
@@ -5,8 +5,20 @@ module MergeRequestsHelper
   include ActionView::Helpers::TagHelper
   include HumanizeHelper
 
-  def user_emoji_character(emoji_name)
-    Emoji.find_by_alias(emoji_name)&.raw if emoji_name
+  def user_emojis(user_status)
+    return unless user_status
+
+    emojis = []
+
+    emojis << if user_status.emoji == "speech_balloon" && user_status.message.scan(/:([\w+-]+):/).size == 1
+      emojify(user_status.message)
+    else
+      user_emoji_character(user_status.emoji)
+    end
+
+    emojis << "🔴" if user_status.availability == "BUSY"
+
+    emojis.uniq.join
   end
 
   def user_country_flag_classes(user)
@@ -68,6 +80,10 @@ module MergeRequestsHelper
       .compact_blank
       .map { |title, value| tag.div("#{tag.b(title)}: #{value}", class: "text-start", escape: false) }
       .join
+  end
+
+  def user_emoji_character(emoji_name)
+    Emoji.find_by_alias(emoji_name)&.raw if emoji_name
   end
 
   def emojify(text)
