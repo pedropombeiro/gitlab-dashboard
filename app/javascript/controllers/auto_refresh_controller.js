e8efceb77dcab233c7198cd90479964b01266178
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-01-07 13:11:37 +0100

    Try to avoid double refresh from auto-refresh-controller

--- app/javascript/controllers/auto_refresh_controller.js
+++ app/javascript/controllers/auto_refresh_controller.js
@@ -5,20 +5,31 @@ export default class extends Controller {
   static values = { timeout: Number, targetDomId: String };
 
   controller = new AbortController();
+  nextRefreshTimestamp = 0;
+  timeoutID = 0;
 
   connect() {
     console.log("Auto refresh controller connected to", this.targetDomIdValue);
 
     const onVisibilityChange = () => {
-      if (document.visibilityState === "visible") {
-        console.log("Auto refreshing after page made visible");
-        this.refresh();
+      if (document.visibilityState !== "visible") {
+        return;
       }
+      if (this.nextRefreshTimestamp === 0) {
+        return;
+      }
+      if (Date.now() < this.nextRefreshTimestamp) {
+        return;
+      }
+
+      console.log("Auto refreshing after page made visible");
+      this.refresh();
     };
 
     document.addEventListener("visibilitychange", onVisibilityChange, false, { signal: this.controller.signal });
 
-    setTimeout(this.refresh.bind(this), this.timeoutValue);
+    this.nextRefreshTimestamp = Date.now() + this.timeoutValue;
+    this.timeoutID = setTimeout(this.refresh.bind(this), this.timeoutValue);
   }
 
   disconnect() {
@@ -26,6 +37,14 @@ export default class extends Controller {
   }
 
   refresh() {
+    if (this.timeoutID !== 0) {
+      // Avoid re-entry caused by visibility change happening when timeout expires
+      clearTimeout(this.timeoutID);
+      this.timeoutID = 0;
+      this.nextRefreshTimestamp = 0;
+      this.controller.abort();
+    }
+
     const element = document.getElementById(this.targetDomIdValue);
     if (element) {
       element.reload();
