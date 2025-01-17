077c686f0caef9d7671d11d11a07d8c2a57c74e2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-01-17 15:07:49 +0100

    Don't raise error when theme-selector button is not present

--- app/javascript/controllers/theme_selector_controller.js
+++ app/javascript/controllers/theme_selector_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
   }
 
   refreshTheme() {
-    if (this.element === null || this.buttonTarget === null) {
+    if (this.element === null) {
       return;
     }
 
@@ -20,10 +20,14 @@ export default class extends Controller {
 
     if (isDark) {
       this.element.setAttribute("data-bs-theme", "dark");
-      this.buttonTarget.innerHTML = '<i class="bi bi-moon-fill"></i>';
+      if (this.hasButtonTarget) {
+        this.buttonTarget.innerHTML = '<i class="bi bi-moon-fill"></i>';
+      }
     } else {
       this.element.setAttribute("data-bs-theme", "light");
-      this.buttonTarget.innerHTML = '<i class="bi bi-sun-fill"></i>';
+      if (this.hasButtonTarget) {
+        this.buttonTarget.innerHTML = '<i class="bi bi-sun-fill"></i>';
+      }
     }
   }
 
