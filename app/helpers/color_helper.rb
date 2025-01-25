ecf6efbbc6410c58dbce01b1380638d5b2046878
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-01-26 00:26:27 +0100

    Show branch icons in deterministic colors

--- app/helpers/color_helper.rb
+++ app/helpers/color_helper.rb
@@ -1,19 +1,37 @@
 # frozen_string_literal: true
 
-module IssueColorHelper
+module ColorHelper
   # Soft, distinct colors that won't be confused with status indicators
   PALETTE = [
     {bg: "#FF6B6B", text_class: "text-white"},   # Vibrant red
     {bg: "#4DABF7", text_class: "text-white"},   # Strong blue
     {bg: "#9775FA", text_class: "text-white"},   # Rich purple
     {bg: "#FF922B", text_class: "text-white"},   # Deep orange
-    {bg: "#51CF66", text_class: "text-black"},   # Rich green
+    {bg: "#51CF66", text_class: "text-white"},   # Rich green
     {bg: "#FF8ED4", text_class: "text-black"},   # Bright pink
     {bg: "#FF9F1C", text_class: "text-black"},   # Bright yellow
     {bg: "#5C7CFA", text_class: "text-white"},   # Royal blue
     {bg: "#BE4BDB", text_class: "text-white"},   # Deep purple
-    {bg: "#20C997", text_class: "text-white"}    # Teal
+    {bg: "#20C997", text_class: "text-white"},   # Teal
+    {bg: "#845EF7", text_class: "text-white"},   # Deep indigo
+    {bg: "#339AF0", text_class: "text-white"},   # Ocean blue
+    {bg: "#F06595", text_class: "text-white"},   # Deep pink
+    {bg: "#94D82D", text_class: "text-black"},   # Lime green
+    {bg: "#FF8787", text_class: "text-black"},   # Coral
+    {bg: "#66D9E8", text_class: "text-black"},   # Sky blue
+    {bg: "#FCC419", text_class: "text-black"},   # Golden yellow
+    {bg: "#E599F7", text_class: "text-black"},   # Light purple
+    {bg: "#FF922B", text_class: "text-white"},   # Burnt orange
+    {bg: "#3BC9DB", text_class: "text-black"}    # Turquoise
   ].freeze
+
+  def branch_color(branch_name)
+    return nil if branch_name.blank?
+
+    # Use the issue number to deterministically select a color
+    index = Zlib.crc32(branch_name, 0) % PALETTE.length
+    PALETTE[index][:bg]
+  end
 
   def issue_color(issue_number)
     return nil if issue_number.nil?
@@ -23,7 +41,6 @@ module IssueColorHelper
     PALETTE[index]
   end
 
-  # Alternative method that uses Bootstrap badges instead of dots
   def issue_badge(issue_number)
     return "" if issue_number.nil?
 
