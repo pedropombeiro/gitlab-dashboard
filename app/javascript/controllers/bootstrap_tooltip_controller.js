b5b3a64c097fa0959c4585bead064736c1d6b022
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-01-27 19:39:02 +0100

    Add popovers with links to handbook

--- app/javascript/controllers/bootstrap_tooltip_controller.js
+++ app/javascript/controllers/bootstrap_tooltip_controller.js
@@ -5,8 +5,11 @@ export default class extends Controller {
   connect() {
     setTimeout(() => {
       // Need to wait for 500 ms since relative_timestamp sets data-bs-toggle class a bit too late
-      var tooltipTriggerList = this.element.querySelectorAll('[data-bs-toggle="tooltip"]');
+      const tooltipTriggerList = this.element.querySelectorAll('[data-bs-toggle="tooltip"]');
       [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+
+      const popoverTriggerList = this.element.querySelectorAll('[data-bs-toggle="popover"]');
+      [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
     }, 500);
   }
 }
