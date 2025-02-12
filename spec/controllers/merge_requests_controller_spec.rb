f6761934b919264c08d0482a1b5df5c0c15ee1f0
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-12 23:27:53 +0100

    Add review state icon to popover

--- spec/controllers/merge_requests_controller_spec.rb
+++ spec/controllers/merge_requests_controller_spec.rb
@@ -147,7 +147,7 @@ RSpec.describe MergeRequestsController, type: :controller do
             %(<a href="javascript:void(0);" role="button" class="fw-normal">#{author}</a>)
           )
           expect(response.body).to include(
-            %(<a target="_blank" href="https://gitlab.example.com/#{author}">#{author}</a>).gsub('"', "&quot;")
+            %(<a target="_blank" href="https://gitlab.example.com/#{author}"><span class="me-1">#{author}</span><i ).gsub('"', "&quot;")
           )
           # and user's avatar
           expect(response.body).to include(
