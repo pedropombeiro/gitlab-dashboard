b83ee4dc09e31244a1c2b2b6944cfcc8dae9072b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-14 00:03:05 +0100

    Code reformat

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,7 +9,7 @@ LocalTime.config.useFormat24 = true;
 LocalTime.start();
 
 document.addEventListener("turbo:frame-missing", async function (event) {
-  event.preventDefault()
+  event.preventDefault();
 
   // Replace the document with whatever was returned in the response, without replacing the URL
   document.open();
@@ -17,19 +17,18 @@ document.addEventListener("turbo:frame-missing", async function (event) {
     document.write(new TextDecoder().decode(chunk));
   }
   document.close();
-})
+});
 
 // Allow table elements in Bootstrap tooltips
-const defaultAllowList = bootstrap.Tooltip.Default.allowList
+const defaultAllowList = bootstrap.Tooltip.Default.allowList;
 
-defaultAllowList.a = ['target', 'href', 'title', 'rel', 'data-action', 'data-bs-toggle', 'data-bs-title']
-defaultAllowList.code = ['data-clipboard-target']
-defaultAllowList.i = []
-defaultAllowList.li = ['data-controller']
-defaultAllowList.span = ['data-controller', 'data-clipboard-target']
-defaultAllowList.table = []
-defaultAllowList.tbody = []
-defaultAllowList.tr = []
-defaultAllowList.td = []
-defaultAllowList.nobr = []
-
+defaultAllowList.a = ["target", "href", "title", "rel", "data-action", "data-bs-toggle", "data-bs-title"];
+defaultAllowList.code = ["data-clipboard-target"];
+defaultAllowList.i = [];
+defaultAllowList.li = ["data-controller"];
+defaultAllowList.span = ["data-controller", "data-clipboard-target"];
+defaultAllowList.table = [];
+defaultAllowList.tbody = [];
+defaultAllowList.tr = [];
+defaultAllowList.td = [];
+defaultAllowList.nobr = [];
