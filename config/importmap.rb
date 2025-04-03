97f9248dcbb3f1a14d442938c774e51c8a301ec3
Pedro Pombeiro <ppombeiro@gitlab.com>
2025-04-03 09:16:45 +0200

    chore(node): Bump packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "local-time" # @3.0.2
+pin "local-time" # @3.0.3
 pin "bootstrap" # @5.3.3
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
