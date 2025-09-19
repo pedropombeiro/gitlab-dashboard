942277eec143dc10783b3e3d766eae80c984cd2b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-09-19 23:03:38 +0200

    fix(rails): Fix lookup of timezone for Philippines

--- config/initializers/timezone.rb
+++ config/initializers/timezone.rb
@@ -3,5 +3,6 @@ geonames_username = Rails.application.credentials.dig(:geonames, :username)
 if geonames_username.present?
   Timezone::Lookup.config(:geonames) do |c|
     c.username = geonames_username
+    c.offset_etc_zones = true
   end
 end
